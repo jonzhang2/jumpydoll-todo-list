fc421527a626a23e46739a5a28a45cf6f7fa56de
jonzhang2 <jonzhang42@gmail.com>
2022-06-28 03:00:56 +0000

    Initial commit

---
+++ src/test/java/com/jumpydoll/app/jonzhang2/jumpydolltodolist/JumpydollTodoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jumpydoll.app.jonzhang2.jumpydolltodolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JumpydollTodoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
